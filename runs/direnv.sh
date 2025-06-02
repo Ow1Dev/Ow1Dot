9a4fe0efbda4365f44836afffb7d547f6078065f
Mads Bruun Jensen <31339803+Ow1Dev@users.noreply.github.com>
2025-06-02 17:15:28 +0200

    feat: adding direnv to run

---
+++ runs/direnv.sh
@@ -0,0 +1,14 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change the working directory to the parent directory of the script
+PARENT_DIR="$SCRIPT_DIR/.."
+cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+
+if ! source "$(dirname "$(readlink -f "$0")")/../utils/functions.sh"; then
+  echo "Failed to source functions.sh"
+  exit 1
+fi
+
+LOG="Install-Logs/install-$(date +%d-%H%M%S)_direnv.log"
+
+install_package_pacman direnv "$LOG"
