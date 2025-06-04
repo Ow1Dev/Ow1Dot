3f8fa886292a2795b9ac1aba9ae3e257ed39cd58
Mads Bruun Jensen <31339803+Ow1Dev@users.noreply.github.com>
2025-06-04 22:27:10 +0200

    feat: adding nix to my config

---
+++ runs/nix.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change the working directory to the parent directory of the script
+PARENT_DIR="$SCRIPT_DIR/.."
+cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+
+if ! source "$(dirname "$(readlink -f "$0")")/../utils/functions.sh"; then
+  echo "Failed to source functions.sh"
+  exit 1
+fi
+
+LOG="Install-Logs/install-$(date +%d-%H%M%S)_nix.log"
+
+install_package_pacman nix "$LOG"
